1e1ad0711255a0e6cbdfab6281b4a053ac426c56
wajahatabbas27 <wajahatabbas27@gmail.com>
2021-10-20 03:12:38 +0500

    completed this complete project working fine till the class

---
+++ functions/bookmark/bookmark.js
@@ -0,0 +1,102 @@
+const { ApolloServer, gql } = require("apollo-server-lambda")
+const faunadb = require("faunadb")
+const dotenv = require("dotenv")
+const q = faunadb.query
+
+dotenv.config()
+
+const typeDefs = gql`
+  type Query {
+    bookmark: [Bookmark!]
+  }
+  type Bookmark {
+    id: ID!
+    url: String!
+    desc: String!
+  }
+  type Mutation {
+    addBookmark(url: String!, desc: String!): Bookmark
+  }
+`
+
+const authors = [
+  {
+    id: 1,
+    url: "https://github.com/daniyalnagori/bookmarking-app/blob/master/gatsby-browser.js",
+    desc: "daniyal nagori repo",
+  },
+  {
+    id: 2,
+    url: "https://dashboard.fauna.com/db/global/bookmarks",
+    desc: "fauna-db dashboard",
+  },
+  {
+    id: 3,
+    url: "https://www.youtube.com/watch?v=FPH7NYrdBgY&t=912s",
+    desc: "bootcamp class project 12-d",
+  },
+]
+
+const resolvers = {
+  Query: {
+    bookmark: async (root, args, context) => {
+      try {
+        const client = new faunadb.Client({
+          secret: process.env.FAUNA_DB_SECRET,
+        })
+        //getting data from faunadb
+        const result = await client.query(
+          q.Map(
+            q.Paginate(q.Match(q.Index("url"))),
+            q.Lambda(x => q.Get(x))
+          )
+        )
+        return result.data.map(d => {
+          return {
+            id: d.ts,
+            url: d.data.url,
+            desc: d.data.desc,
+          }
+        })
+        return authors
+      } catch (error) {
+        console.log("error", error)
+      }
+    },
+  },
+  Mutation: {
+    addBookmark: async (_, { url, desc }) => {
+      try {
+        const client = new faunadb.Client({
+          secret: process.env.FAUNA_DB_SECRET,
+        })
+        var result = await client.query(
+          q.Create(q.Collection("links"), {
+            data: {
+              url,
+              desc,
+            },
+          })
+        )
+        console.log(
+          "Document Created and Inserted in Container: " + result.ref.id
+        )
+        //console.log("url :", url, "desc :", desc)
+
+        return result.ref.data
+      } catch (error) {
+        console.log("Error: ")
+        console.log(error)
+      }
+    },
+  },
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+const handler = server.createHandler()
+
+module.exports = { handler }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,57 @@
 import React from "react"
+import { useQuery, useMutation } from "@apollo/client"
+import gql from "graphql-tag"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const BOOKMARK_QUERY = gql`
+  {
+    bookmark {
+      id
+      url
+      desc
+    }
+  }
+`
+
+//mutations
+const ADD_BOOKMARK_MUTATION = gql`
+  mutation addBookmark($url: String!, $desc: String!) {
+    addBookmark(url: $url, desc: $desc) {
+      url
+    }
+  }
+`
+
+const Index = () => {
+  let textfield
+  let desc
+  const { loading, error, data } = useQuery(BOOKMARK_QUERY)
+  const [addBookmark] = useMutation(ADD_BOOKMARK_MUTATION)
+  //console.log(data)
+  const addBookmarkSubmit = () => {
+    addBookmark({
+      variables: {
+        url: textfield.value,
+        desc: desc.value,
+      },
+      //refetching data using mutation provided by graphql
+      refetchQueries: [{ query: BOOKMARK_QUERY }],
+    })
+    console.log("textfield :", textfield.value, "desc :", desc.value)
+  }
+  return (
+    <div>
+      <p>{JSON.stringify(data)}</p>
+      <div>
+        <input type="text" placeholder="URL" ref={node => (textfield = node)} />
+        <input
+          type="text"
+          placeholder="description"
+          ref={node => (desc = node)}
+        />
+        <button onClick={addBookmarkSubmit}>Add Bookmark</button>
+      </div>
+    </div>
+  )
 }
+
+export default Index
